3753b3fd15288381fcd32929961e7c2dbe82fec7
zwarott <zwarott@email.cz>
2023-07-18 22:48:37 +0200

    update functions - "update_col" and "update_cols"

--- src/squad/controllers.py
+++ src/squad/controllers.py
@@ -2,6 +2,7 @@ from time import time
 
 import psycopg2 as ps
 from csv import DictReader
+import pandas as pd
 from sqlalchemy import update
 from flask_sqlalchemy import DefaultMeta
 
@@ -11,7 +12,7 @@ from __init__ import db
 def csv_to_table(csv_path: str, db_table: str, commit_changes: bool = True) -> None:
     """Import data from csv file.
 
-    Import data from csv file into PostgreSQL database using
+    Import data from csv file to empty table in PostgreSQL database using
     psycopg2 module.
 
     Parameters
@@ -84,7 +85,7 @@ def update_col(csv_path: str, obj: DefaultMeta, col: str, pk_col: str) -> None:
     start_time = time()
     with open(csv_path, "r", encoding="utf-8") as f:
         reader = DictReader(f)
-        # Create list of dictionaries for updating.
+        # Create list of dictionaries.
         # to_update[n][pk_col] -> pk_col value
         # to_update[n][col] -> col value
         to_update = [{pk_col: d[pk_col], col: d[col]} for d in reader]
@@ -94,4 +95,38 @@ def update_col(csv_path: str, obj: DefaultMeta, col: str, pk_col: str) -> None:
     duration = time() - start_time
     # Commit changes to the database.
     db.session.commit()
-    return print(f"Importing time: {duration:.2f} s")
+    print(f"Importing time: {duration:.2f} s")
+
+
+def update_cols(csv_path: str, obj: DefaultMeta, cols: list) -> None:
+    """Update selected columns from csv file.
+
+    Update data in selected columns within specific table
+    in PostgreSQL database.
+
+    Parameters
+    ----------
+    csv_path : str
+        A string representing path to csv file.
+    obj : DefaultMeta
+        Class representing table in PostgreSQL database.
+    cols: list
+        List of selected columns to update. Make sure that
+        column with primary key is included.
+
+    Usage Example
+    -------------
+    >>> update_cols("./src/squad/data/first_team.csv", Player, ["number", "last_name"])
+    """
+    start_time = time()
+    # Create dataframe with selected columns.
+    df = pd.read_csv(csv_path, usecols=cols)
+    # Create list of dictionaries.
+    to_update = df.to_dict("records")
+
+    # Update existing values to the database.
+    db.session.execute(update(obj), to_update)
+    duration = time() - start_time
+    # Commit changes to the database.
+    db.session.commit()
+    print(f"Importing time: {duration:.2f} s")
